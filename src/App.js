ab57c8d54297a745e859f2ca9bec69310e9bd2e3
Thomakiin <Thomakiin@gmail.com>
2021-06-17 10:28:48 -0400

    added core features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+var textToEnter = 'Enter this text!';
+var currentIndex = 0; // index of the text we need to enter
+
 function App() {
+  const [currentText, setCurrentText] = useState('');
+  const [pressedKey, setPressedKey] = useState('');
+
+  document.onkeypress = handleKeyPress;
+
+  function handleKeyPress(e) {
+    setPressedKey(e.key);
+    var letterToEnter = textToEnter.charAt(currentIndex);
+
+
+    if (e.key === letterToEnter) {
+      setCurrentText(currentText + e.key);
+      currentIndex = currentIndex + 1;
+      console.log("currentIndex =" + currentIndex);
+    }
+    else {
+      console.log(e.key + " incorrect key entered!");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h3>Typing Game!</h3>
+        <p id="textToEnter">
+          {textToEnter}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>Your text: </h2>
+        <p className="currentText">
+          {currentText}
+        </p>
+
+        <h2>{pressedKey}</h2> 
+
       </header>
     </div>
   );
