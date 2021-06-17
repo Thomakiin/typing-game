5e9e7a92dff33e8593f946ad98ff5a549a233c74
Thomakiin <Thomakiin@gmail.com>
2021-06-17 16:21:50 -0400

    Added random quote functionality, added style animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import { useState } from 'react';
 import './App.css';
+import {quotes} from './quotes.json';
 
-var textToEnter = 'Enter this text!';
+//var textToEnter = 'Enter this text!';
 var currentIndex = 0; // index of the text we need to enter
 
 function App() {
   const [currentText, setCurrentText] = useState('');
   const [pressedKey, setPressedKey] = useState('');
+  const [textToEnter, setTextToEnter] = useState(quotes[Math.floor(Math.random()*quotes.length)].quote);
 
   document.onkeypress = handleKeyPress;
 
   function handleKeyPress(e) {
     setPressedKey(e.key);
     var letterToEnter = textToEnter.charAt(currentIndex);
-
+    console.log("letterToEnter: " + letterToEnter);
+    var el = document.getElementById("pressedKey");
+    el.classList.remove("keyPressAnimClass");
+    void el.offsetWidth;
+    el.classList.add("keyPressAnimClass");
 
     if (e.key === letterToEnter) {
       setCurrentText(currentText + e.key);
-      currentIndex = currentIndex + 1;
+      currentIndex++;
       console.log("currentIndex =" + currentIndex);
+      if (currentIndex >= textToEnter.length) {
+        alert("You win!");
+        console.log("Completed!");
+        setTextToEnter(quotes[Math.floor(Math.random()*quotes.length)].quote);
+        currentIndex = 0;
+      }
     }
     else {
       console.log(e.key + " incorrect key entered!");
@@ -33,12 +45,11 @@ function App() {
           {textToEnter}
         </p>
 
-        <h2>Your text: </h2>
         <p className="currentText">
           {currentText}
         </p>
-
-        <h2>{pressedKey}</h2> 
+        
+        <h2 id="pressedKey" className="pressedKey">{pressedKey}</h2>
 
       </header>
     </div>
